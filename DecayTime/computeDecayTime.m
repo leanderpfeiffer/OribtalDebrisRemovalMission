58275d5e595d86877490fa029dfd7a0ab36d2905
Leander Pfeiffer <leander.pfeiffer@tum.de>
2022-09-19 11:51:48 +0200

    Added simulation for existing Debris

--- DecayTime/computeDecayTime.m
+++ DecayTime/computeDecayTime.m
@@ -1,9 +1,34 @@
-decaytime = 0;
-B = getBallisticCoefficient(0.4, 5^2*pi, 10000);
-h = 200000;
+B = 0.02; %getBallisticCoefficient(0.6, 0.1, 1.33);
 
-while h > 0
-    [dt, hNew] =  getDecayTimeForLevel(h,B);
-    decaytime = decaytime + dt/(3600*24*365) % in hours
-    h = hNew - 1 
+tGoal = 25; % [years]
+
+hA = 650000;
+hB = 50000;
+tol = 1; % [years]
+
+tNew = getDecayTime(B,hA);
+
+while tNew > tGoal + tol || tNew < tGoal - tol
+   hNew = (hA + hB)/2;
+   tNew = getDecayTime(B,hNew);
+   if tNew - tGoal > 0
+       hA = hNew;
+   elseif tNew -tGoal < 0
+       hB = hNew;
+   end
 end
+
+hA
+tNew
+
+
+
+function t = getDecayTime(B,h)
+    t = 0;
+    while h > 0
+        [dt, hNew] =  getDecayTimeForLevel(h,B);
+        t = t + dt/(3600*24*365); % in years
+        h = hNew - 1; 
+    end
+end
+
