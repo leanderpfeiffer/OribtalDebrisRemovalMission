855f554bd950d4d1d1c5a9cd54159a56c10af689
Leander Pfeiffer <leander.pfeiffer@tum.de>
2022-09-26 20:44:44 +0200

    Optimized initial Orbit with current deltaV requirements

--- StalliteOperations/optimizeInitalOrbit.m
+++ StalliteOperations/optimizeInitalOrbit.m
@@ -1,17 +1,26 @@
+createSatellites
 load satellites.mat sats startOrbit
 
-RAANs = 0:1:50;
-maxWeight = 100000;
-optRAAN = 0;
+%RAANs = 0:1:50;
+%semiMajorAxes = (6571:20:7600)*1e+03;
+e = 0:0.01:0.5;
+%inclination = 95:0.05:100;
+%argOfPeri = -10:0.2:20;
 
-for i = 1:length(RAANs)
-    startOrbit.RAAN = RAANs(i);
+len = length(e);
+
+maxWeight = zeros(1,len);
+
+for i = 1:len
+    %startOrbit.RAAN = RAANs(i);
+    %startOrbit.semiMajorAxis = semiMajorAxes(i);
+    startOrbit.eccentricity = e(i);
+    %startOrbit.inclination = inclination(i);
+    %startOrbit.argOfPeri = argOfPeri(i);
+    
     [deltaVCatch, deltaVSlowDown, massOfFuel, massAtStart, durations] = computeDeltaV(sats, startOrbit);
-    maxWeightCheck = max(massAtStart);
-    if maxWeight > maxWeightCheck
-        maxWeight = maxWeightCheck;
-        optRAAN = RAANs(i);
-    end
+    maxWeight(i) = max(massAtStart);
 end
 maxWeight
-optRAAN
+bestWeight = min(maxWeight)
+value = e(find(maxWeight == bestWeight))
